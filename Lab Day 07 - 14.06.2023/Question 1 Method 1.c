9aef88560f2695754c35b9c2549349374d9b4ff1
Dulan2002 <135966685+Dulan2002@users.noreply.github.com>
2023-06-18 20:28:21 +0530

    Delete Lab Day 07 - 14.06.2023 directory

--- Lab Day 07 - 14.06.2023/Question 1 Method 1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a;
-    printf("Enter An Integer - ");
-    scanf("%d",&a);
-    if(a%2==0){printf("The Number Entered Is An Even Number");}
-    else{printf("The Number Entered Is A Odd  Number");}
-}
